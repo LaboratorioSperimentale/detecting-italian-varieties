a1e4d37cb2c41afd1b728046dccdbcf17e8f5a3b
Ludovica Pannitto <ellepannitto@gmail.com>
2024-05-08 12:34:21 +0200

    new traits

--- div/extractor.py
+++ div/extractor.py
@@ -1,13 +1,51 @@
-import re
+# import re
+import regex as re
 
 from pathlib import Path
 from typing import List
 
 from div.utils import data_reader as du
 
-def extract_voc(voc_list: List[str], 
-				context_width: int = 200) -> None:
-	files_list = list(Path("data/KIPasti_txt").glob("*.txt"))
+def extract_voc(files_list: List[Path], voc_list: List[str],
+				context_width: int = 200):
+
+	ret = []
+
+	for file in files_list:
+		turns = du.create_turns(file)
+		for turn in turns:
+			conv, speaker, turn = turn
+			conflated_turn = " "
+			for subturn in turn:
+				for x in subturn:
+					conflated_turn+=f"{x} "
+
+
+			# print(conflated_turn, len(conflated_turn))
+			for element in voc_list:
+				pattern = re.compile(r" {} ".format(element))
+
+				occs = [m.start() for m in re.finditer(pattern, conflated_turn, overlapped=True)]
+
+
+				if len(occs)>0:
+					# print(occs)
+					# input()
+					for occ in occs:
+						prev_ctx = conflated_turn[max(0, occ-context_width):occ]
+						ctx = conflated_turn[occ:occ+len(element)+1]
+						next_ctx = conflated_turn[occ+len(element)+1:min(len(conflated_turn), occ+len(element)+1+context_width)]
+
+						ret.append((conv, speaker, prev_ctx, ctx, next_ctx))
+
+	return ret
+
+
+def extract_pattern(files_list: List[Path], voc_list: List[str],
+					voc_to_filter: List[str],
+					context_width: int = 200):
+
+	ret = []
 
 	for file in files_list:
 		turns = du.create_turns(file)
@@ -19,19 +57,96 @@ def extract_voc(voc_list: List[str],
 					conflated_turn+=f"{x} "
 
 			for element in voc_list:
+				pattern = re.compile(r"{} ".format(element))
+
+				occs = [m.span() for m in re.finditer(pattern, conflated_turn, overlapped=True)]
+
+				if len(occs)>0:
+
+					for occ in occs:
+						occ_start, occ_end = occ
+						occ_len = occ_end - occ_start
+
+						prev_ctx = conflated_turn[max(0, occ_start-context_width):occ_start]
+						ctx = conflated_turn[occ_start:occ_end]
+						next_ctx = conflated_turn[occ_end:min(len(conflated_turn), occ_end+context_width)]
+
+						# prev_ctx = prev_ctx.split(" ")
+						# prev_ctx = [x.split("/")[0] for x in prev_ctx]
+						# prev_ctx = " ".join(prev_ctx)
+
+						# ctx = ctx.split(" ")
+						# ctx = [x.split("/")[0] for x in ctx]
+						# ctx = " ".join(ctx)
+
+						# next_ctx = next_ctx.split(" ")
+						# next_ctx = [x.split("/")[0] for x in next_ctx]
+						# next_ctx = " ".join(next_ctx)
+
+
+						# prev_ctx = conflated_turn[max(0, occ-context_width):occ].split(" ")
+						# ctx = prev_ctx[-1]+conflated_turn[occ:occ+len(element)+1]
+						# next_ctx = conflated_turn[occ+len(element)+1:min(len(conflated_turn), occ+len(element)+1+context_width)]
+
+						# if len(prev_ctx[-1])>1 and not prev_ctx[-1] in voc_to_filter:
+						if not ctx.strip() in voc_to_filter:
+							ret.append((conv, speaker, prev_ctx, ctx, next_ctx))
+
+	return ret
+
+
+def extract_lemma(files_list: List[Path], voc_list: List[str],
+				  context_width: int = 200):
+
+	ret = []
+
+	for file in files_list:
+		turns = du.create_turns_conll(file)
+
+		for turn in turns:
+			conv, speaker, turn = turn
+			# print(conv, speaker, turn)
+			# input()
+			conflated_turn = " "
+			for subturn in turn:
+				for form, lemma, pos in subturn:
+					conflated_turn+=f"{form}/{lemma}/{pos} "
+
+
+			# print(conflated_turn, len(conflated_turn))
+			# input()
+
+			for element in voc_list:
+
 				pattern = re.compile(r" {} ".format(element))
 
-				occs = [m.start() for m in re.finditer(pattern, conflated_turn)]
-				if len(occs)>0:
-					
-					for occ in occs:
-						prev_ctx = conflated_turn[max(0, occ-context_width):occ]
-						ctx = conflated_turn[occ:occ+len(element)+1]
-						next_ctx = conflated_turn[occ+len(element)+1:min(len(conflated_turn), occ+len(element)+1+context_width)]
+				occs = [m.span() for m in re.finditer(pattern, conflated_turn, overlapped=True)]
 
-						print(element)
-						print(conflated_turn)
-						print("PREV", prev_ctx)
-						print(ctx)
-						print("NEXT", next_ctx)
-						input()
+				if len(occs)>0:
+					# print(element)
+					# print(occs)
+					# input()
+
+					for occ in occs:
+						occ_start, occ_end = occ
+						occ_len = occ_end - occ_start
+
+						prev_ctx = conflated_turn[max(0, occ_start-context_width):occ_start]
+						ctx = conflated_turn[occ_start:occ_end]
+						next_ctx = conflated_turn[occ_end:min(len(conflated_turn), occ_end+context_width)]
+
+						prev_ctx = prev_ctx.split(" ")
+						prev_ctx = [x.split("/")[0] for x in prev_ctx]
+						prev_ctx = " ".join(prev_ctx)
+
+						ctx = ctx.split(" ")
+						ctx = [x.split("/")[0] for x in ctx]
+						ctx = " ".join(ctx)
+
+						next_ctx = next_ctx.split(" ")
+						next_ctx = [x.split("/")[0] for x in next_ctx]
+						next_ctx = " ".join(next_ctx)
+
+						ret.append((conv, speaker, prev_ctx, ctx, next_ctx))
+
+	return ret
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 from pathlib import Path
 
+import pandas as pd
+
 import yaml
 
 from yaml import load
@@ -20,20 +22,49 @@ def _parse_data(args):
     input_files = list(input_dir.glob("*.txt"))
 
     du.parse_data(input_files, args.output_dir)
-    
+
 
 def _extract_feats(args):
 
+
     dictionary = yaml.load(open(args.features), Loader)
+    input_folder_KIP = Path(dictionary["input_folder"]["KIP"])
+    input_folder_KIPasti = Path(dictionary["input_folder"]["KIPasti"])
+
+    output_folder = Path(dictionary["output_folder"])
 
     tratti = dictionary["tratti"]
+
     for tratto in tratti:
         name = None
         type = tratto["type"]
 
         if type == "VOC":
-            extractor.extract_voc(tratto["accepted_elements"])
-    
+            files_list = [input_folder_KIP.joinpath(f"{x}.txt") for x in dictionary["selected_conversations"]["KIP"]] + [input_folder_KIPasti.joinpath(f"{x}.txt") for x in dictionary["selected_conversations"]["KIPasti"]]
+            ret = extractor.extract_voc(files_list, tratto["accepted_elements"])
+
+        elif type == "PATTERN":
+            files_list = [input_folder_KIP.joinpath(f"{x}.txt") for x in dictionary["selected_conversations"]["KIP"]] + [input_folder_KIPasti.joinpath(f"{x}.txt") for x in dictionary["selected_conversations"]["KIPasti"]]
+            ret = extractor.extract_pattern(files_list, tratto["accepted_elements"],
+                                            tratto["filter_elements"])
+
+        elif type == "LEMMA":
+            files_list = [input_folder_KIP.joinpath(f"{x}.conllu") for x in dictionary["selected_conversations"]["KIP"]] + [input_folder_KIPasti.joinpath(f"{x}.conllu") for x in dictionary["selected_conversations"]["KIPasti"]]
+            ret = extractor.extract_lemma(files_list, tratto["accepted_elements"])
+
+
+
+
+        sorted_ret = list(sorted(ret, key=lambda x: x[3]))
+        with open(output_folder.joinpath(f"{tratto['type']}_{tratto['name']}.tsv"), "w") as fout:
+            print("CONV\tSPEAKER\tPRE_CTX\tHIT\tPOST_CTX", file=fout)
+            for r in sorted_ret:
+                print("\t".join(r), file=fout)
+
+
+
+        df1 = pd.DataFrame(sorted_ret, columns=["CONV", "SPEAKER", "PRE_CTX", "HIT", "POST_CTX"])
+        df1.to_excel(output_folder.joinpath(f"{tratto['type']}_{tratto['name']}.xlsx"))
 
 if __name__ == "__main__":
 
@@ -54,6 +85,7 @@ if __name__ == "__main__":
     # parser_processdata.add_argument("--type", default="KIP", choices=["KIP", "KIPasti"])
     parser_processdata.set_defaults(func=_parse_data)
 
+
     parser_extractfeatures = subparsers.add_parser('extract', parents=[parent_parser],
                                                     description='extract features',
                                                     help='extract features')
@@ -62,6 +94,7 @@ if __name__ == "__main__":
     parser_extractfeatures.add_argument("-f", "--features", required=True,
                                         help="path to features cfg file")
     parser_extractfeatures.set_defaults(func=_extract_feats)
+
 
     args = root_parser.parse_args()
 
